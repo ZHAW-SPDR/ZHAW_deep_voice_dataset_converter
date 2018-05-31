87925213fdbee6be91b890ea1ea0b0091941dcef
Amin <amintrabi@hotmail.com>
2018-05-31 16:35:54 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ZHAW_deep_voice_dataset_converter',
+    version='',
+    packages=['rt09'],
+    url='',
+    license='',
+    author='Niclas Simmler and Amin Trabi',
+    author_email='',
+    description='',
+    install_requires=['numpy', 'pydub', 'yaml']
+)
